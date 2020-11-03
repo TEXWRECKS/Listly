8c13487848d4bcaed1c626c21baec999009eb95e
Emily Aguirre <emgal.aguirre@gmail.com>
2020-11-02 20:35:46 -0500

    updated some css

--- public/js/list.js
+++ public/js/list.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // listContainer holds all of our posts
   var listContainer = $(".list-container");
   var postUrgencySelect = $("#urgency");
@@ -8,7 +8,7 @@ $(document).ready(function() {
   postUrgencySelect.on("change", handleUrgencyChange);
   var posts;
   var dueDate;
-  
+
 
   // This function grabs posts from the database and updates the view
   function getPosts(urgency) {
@@ -16,7 +16,7 @@ $(document).ready(function() {
     if (urgencyString) {
       urgencyString = "/urgency/" + urgencyString;
     }
-    $.get("/api/posts" + urgencyString, function(data) {
+    $.get("/api/posts" + urgencyString, function (data) {
       console.log("Posts", data);
       posts = data;
       if (!posts || !posts.length) {
@@ -34,7 +34,7 @@ $(document).ready(function() {
       method: "DELETE",
       url: "/api/posts/" + id
     })
-      .then(function() {
+      .then(function () {
         getPosts(postUrgencySelect.val());
       });
   }
@@ -54,54 +54,68 @@ $(document).ready(function() {
 
   // This function constructs a post's HTML
   function createNewRow(post) {
-    if (post.urgency === "low"){
-    dueDate = 120;
-  }
-    else if (post.urgency === "medium"){
+    if (post.urgency === "low") {
       dueDate = 60;
     }
-    else if (post.urgency === "high"){
+    else if (post.urgency === "medium") {
       dueDate = 30;
+    }
+    else if (post.urgency === "high") {
+      dueDate = 7;
     }
     var newPostCard = $("<div>");
     newPostCard.addClass("card");
     var newPostCardHeading = $("<div>");
     newPostCardHeading.addClass("card-header");
     var deleteBtn = $("<button>");
-    deleteBtn.text("x");
+    deleteBtn.text("✓");
     deleteBtn.addClass("delete btn btn-danger");
     var editBtn = $("<button>");
     editBtn.text("EDIT");
     editBtn.addClass("edit btn btn-default");
     var newPostTitle = $("<h2>");
+    newPostTitle.addClass("title");
     var newPostDate = $("<small>");
+    newPostDate.addClass("date");
     var newPostUrgency = $("<h5>");
+    newPostUrgency.addClass("urgency");
     newPostUrgency.text(post.urgency);
     newPostUrgency.css({
-      float: "right",
-      "font-weight": "700",
-      "margin-top":
-      "-15px"
+      float: "right"
     });
     var newPostCardBody = $("<div>");
     newPostCardBody.addClass("card-body");
+    newPostCardBody.append(deleteBtn);
+    newPostCardBody.append(editBtn);
     var newPostBody = $("<p>");
     newPostTitle.text(post.title + " ");
     newPostBody.text(post.body);
     var formattedDate = new Date(post.createdAt);
-    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+    formattedDate = moment(formattedDate).format("MMMM Do, YYYY");
     newPostDate.text(formattedDate);
     newPostTitle.append(newPostDate);
-    newPostCardHeading.append(deleteBtn);
-    newPostCardHeading.append(editBtn);
+    newPostTitle.append(newPostUrgency);
+
+
+
     newPostCardHeading.append(newPostTitle);
-    newPostCardHeading.append(newPostUrgency);
+    // newPostCardHeading.append(newPostUrgency);
     newPostCardBody.append(newPostBody);
     newPostCard.append(newPostCardHeading);
     newPostCard.append(newPostCardBody);
     newPostCard.data("post", post);
     return newPostCard;
   }
+
+  // conencting urgency level to date 
+  // var lowUrgency = newmoment().add(2, 'month');
+  // var mediumUrgency = newmoment().add(30, 'month');
+  // var highUrgency = newmoment().add(1, 'week');
+
+  // $("lowUrgency-date").text(lowUrgency.format("MMMM Do, YYYY")
+  // $("mediumUrgency-date").text(mediumUrgency.format("MMMM Do, YYYY")
+  // $("highUrgency-date").text(Urgency.format("MMMM Do, YYYY")
+
 
   // This function figures out which post we want to delete and then calls
   // deletePost
@@ -126,10 +140,11 @@ $(document).ready(function() {
   // This function displays a message when there are no posts
   function displayEmpty() {
     listContainer.empty();
-    var messageH2 = $("<h2>");
+    var messageH2 = $("<h6>");
     messageH2.css({ "text-align": "center", "margin-top": "50px" });
-    messageH2.html("No list yet for this urgency, navigate <a href='/todo'>here</a> in order to create a new list.");
+    messageH2.html("Need to add a task? <br> Let's get started. <br> <a class='here' href='/todo'>New Task</a>");
     listContainer.append(messageH2);
+    messageH2.addClass("messageH2");
   }
 
   // This function handles reloading new posts when the urgency changes
